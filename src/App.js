f07b3d8c1435bcc8792080ddbcd2f4ffec7e2e41
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-08-09 12:37:41 +0300

    user tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+import image from "./african-american-woman-programmer-girl-coding-on-computer-2GNCKPR.jpg";
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Ryne</h1>
+      <img src={image} alt="a girl coding" className="image" />
+      <p>This is my biography</p>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,29 @@
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
 import App from "../App";
 
-// Your tests here
+//Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+  //Arrange
+  render(<App />);
+  //Act
+  const topLevelHeading = screen.getByRole("heading", {
+    name: /hi, i'm/i,
+    exact: false,
+    level: 1,
+  });
+  expect(topLevelHeading).toBeInTheDocument();
+});
+
+test("display an image with the alt `a girl coding", () => {
+  render(<App />);
+  const imageElement = screen.getByAltText(/a girl coding/i);
+  expect(imageElement).toBeInTheDocument();
+});
+
+test("displays a paragraph with the text ` This is my biography`", () => {
+  render(<App />);
+  const paragraphElement = screen.getByText(/This is my biography/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
